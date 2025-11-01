e067cc6ccabbc6dae64d3bd114596059fca8b968
Kachi Alex <opd.livmind@gmail.com>
2025-11-01 08:37:42 +0100

    Fix missing FolderOpen import in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,8 @@ import {
   Calendar, 
   DollarSign,
   User,
-  MoreHorizontal
+  MoreHorizontal,
+  FolderOpen
 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
